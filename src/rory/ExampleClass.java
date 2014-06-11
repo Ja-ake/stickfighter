6ddedd07752e7d5432b13f06fb723df6a413cd00
Jake Springer <jake1springer@gmail.com>
2014-06-10 17:44:14 -0700

    Added each individual package

---
+++ src/rory/ExampleClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rory;
+
+/**
+ *
+ * @author Jake
+ */
+public class ExampleClass {
+    // Have your classes here that you are working on, and when you have finished them move them to the actual package that they will be in
+}
